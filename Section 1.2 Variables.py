69fa9772c1e74859f5919d106663ea04ae31ea88
Christian050 <cristiancolley@gmail.com>
2022-11-28 08:05:50 +0000

    Section 1.2 Updated

--- Section 1.2 Variables.py
+++ Section 1.2 Variables.py
@@ -144,20 +144,20 @@ print(a, b)
 
 # Cascading Assignments:
 
-a = b = c = 1  # all three names a, b and c refer to same int object with value 1
+a = b = c = 1                   # all three names a, b and c refer to same int object with value 1
 print(a, b, c)
 # Output: 1 1 1
 
-b = 2  # b now refers to another int object, one with a value of 2
+b = 2                            # b now refers to another int object, one with a value of 2
 print(a, b, c)
 # Output: 1 2 1  # so output is as expected.
 
 # The above is also true for mutable types (like list, dict, etc.) just as it is true for immutable types (like int, string,
 # tuple, etc.):
 
-x = y = [7, 8, 9]   # x and y refer to the same list object just created, [7, 8, 9]
-x = [13, 8, 9]   # x now refers to a different list object just created, [13, 8, 9]
-print(y)    # y still refers to the list it was first assigned
+x = y = [7, 8, 9]                       # x and y refer to the same list object just created, [7, 8, 9]
+x = [13, 8, 9]                          # x now refers to a different list object just created, [13, 8, 9]
+print(y)                                # y still refers to the list it was first assigned
 # Output: [7, 8, 9]
 
 
@@ -166,10 +166,10 @@ print(y)    # y still refers to the list it was first assigned
 # a different object, which we did above) when the cascading assignment is used for mutable types. Take a look
 # below, and you will see it first hand:
 
-x = y = [7, 8, 9]    # x and y are two different names for the same list object just created, [7, 8, 9]
-x[0] = 13    # we are updating the value of the list [7, 8, 9] through one of its names, x in this case
-print(y)     # printing the value of the list using its other name
-# Output: [13, 8, 9]    # hence, naturally the change is reflected
+x = y = [7, 8, 9]                       # x and y are two different names for the same list object just created, [7, 8, 9]
+x[0] = 13                               # we are updating the value of the list [7, 8, 9] through one of its names, x in this case
+print(y)                                # printing the value of the list using its other name
+# Output: [13, 8, 9]                    # hence, naturally the change is reflected
 
 
 # Nested lists are also valid in python. This means that a list can contain another list as an element.
